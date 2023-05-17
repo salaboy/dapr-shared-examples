82d4684f85a8b96075069bc06e28811341c65212
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-05-16 22:11:10 -0300

    rename matheuscruzdev to salaboy

--- hack/build_and_push.sh
+++ hack/build_and_push.sh
@@ -3,7 +3,6 @@
 build_and_deploy() {
     local folder=$1
     local tag=$2
-
     cd "$folder" || exit
     go mod tidy
     go build main.go
@@ -13,8 +12,8 @@ build_and_deploy() {
     cd ..
 }
 
-build_and_deploy "read-values" "matheuscruzdev/read-values:latest"
-build_and_deploy "subscriber" "matheuscruzdev/subscriber:latest"
-build_and_deploy "write-values" "matheuscruzdev/write-values:latest"
+build_and_deploy "read-values" "salaboy/read-values:latest"
+build_and_deploy "subscriber" "salaboy/subscriber:latest"
+build_and_deploy "write-values" "salaboy/write-values:latest"
 
 kubectl apply -f apps
